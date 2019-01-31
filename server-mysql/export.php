ce9655c4f36671b7497b5cff7b6de69e400d19fa
Honza S. Copak <bonum.comunae@volny.cz>
2019-01-31 16:29:15 +0100

    Content-Type: application/json

--- server-mysql/export.php
+++ server-mysql/export.php
@@ -1,4 +1,5 @@
 <?
+header("Content-Type: application/json; charset=UTF-8");
 
 if (IsSet($_GET["sel"])):$sel=$_GET["sel"];else:$sel="device";endif;
 if (IsSet($_GET["place"])):$place=$_GET["place"];else:$place="none";endif;
@@ -8,7 +9,6 @@ if (IsSet($_GET["limit"])):$limit=$_GET["limit"];else:$limit="200";endif;
 
 include_once 'my_db17.php';
 include_once 'mysql_class.php';
-
 
 //error_reporting(0);
 
@@ -40,25 +40,13 @@ while($row = $queryTable->fetch()) {
  
     /*$vznik = Date("Y-m-d",$row["id"]);
     $vznikH = Date("H:i",$row["id"]);
-    echo "<td>".$row["id"]."</td>";
-    echo "<td>$vznik | $vznikH</td>";
-    //echo "<td>".$row["place"]."</td>";
-    $rowplace=$row["place"];
-    echo "<td><a href=http://www.octopusengine.org//api/hydrop?sel=place&place=$rowplace>$rowplace</a></td>";
-    $sn=$row["device"];
-    echo "<td><a href=http://www.octopusengine.org//api/hydrop?sel=device&device=$sn>$sn</a></td>";
-    $rowtype = $row["type"];
-    echo "<td><a href=http://www.octopusengine.org//api/hydrop?sel=type&type=$rowtype>$rowtype</a></td>";
-
-    $val =  $row["value"];
-    echo "<td>$val</td>";
-  
     echo "<br />";*/
     $json_array['id']=$row['id'];  
     $json_array['place']=$row['place'];  
     $json_array['device']=$row['device'];
     $json_array['type']=$row['type'];
     $json_array['value']=$row['value']; 
+    
     array_push($json_data,$json_array);     
     }
     
