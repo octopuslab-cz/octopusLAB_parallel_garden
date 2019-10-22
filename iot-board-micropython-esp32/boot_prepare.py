aab8bdd93819f8df04e57f8e8698079fef77c85e
Petr Kracík <petrkr@petrkr.net>
2019-10-22 11:10:44 +0200

    Clean up old code

--- iot-board-micropython-esp32/boot_prepare.py
+++
@@ -1,8 +0,0 @@
-# This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-#import webrepl
-#webrepl.start()
-
-# when user enters REPL and executes setup()
-from util.setup import setup
